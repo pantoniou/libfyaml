6589d18f468a4496ee95aba63986eef0b5475b67
Nisal Bandara <nisal.bandara@outlook.com>
2021-05-18 12:31:42 +0530

    Issue #30: #warn -> #warning in lib/fy-parse.c

--- src/lib/fy-parse.c
+++ src/lib/fy-parse.c
@@ -9,19 +9,19 @@
 #include "config.h"
 #endif
 
-#include <stdio.h>
-#include <string.h>
-#include <sys/mman.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/ioctl.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include <assert.h>
-#include <stdlib.h>
 #include <errno.h>
-#include <stdarg.h>
+#include <fcntl.h>
 #include <limits.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #include <libfyaml.h>
 
@@ -37,7 +37,7 @@
 const char *fy_library_version(void)
 {
 #ifndef VERSION
-#warn No version defined
+#warning No version defined
 	return "UNKNOWN";
 #else
 	return VERSION;
