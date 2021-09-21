f72a682cf49d0fbdb588b4b4c0a229f79d0d063e
Pantelis Antoniou <pantelis.antoniou@konsulko.com>
2021-09-21 16:58:43 +0300

    Parcel out fy-emit-accum and api changes
    Take out the accumulator out of the emitter in order to be used
    by other modules.
    
    Signed-off-by: Pantelis Antoniou <pantelis.antoniou@konsulko.com>

--- src/lib/fy-emit.h
+++ src/lib/fy-emit.h
@@ -20,6 +20,7 @@
 
 #include "fy-utf8.h"
 #include "fy-event.h"
+#include "fy-emit-accum.h"
 
 #define FYEF_WHITESPACE			0x0001
 #define FYEF_INDENTATION		0x0002
@@ -31,19 +32,6 @@
 struct fy_document;
 struct fy_emitter;
 struct fy_document_state;
-
-#define FYEA_INPLACE_SZ	256
-struct fy_emit_accum {
-	struct fy_emitter *emit;
-	char *accum;
-	size_t alloc;
-	size_t next;
-	char inplace[FYEA_INPLACE_SZ];
-	int utf8_count;
-	int start_col, col;
-	int ts;
-	enum fy_emitter_write_type type;
-};
 
 enum fy_emitter_state {
 	FYES_NONE,		/* when not using the raw emitter interface */
@@ -94,6 +82,7 @@ struct fy_emitter {
 	struct fy_document *fyd;
 	struct fy_document_state *fyds;	/* fyd->fyds when fyd != NULL */
 	struct fy_emit_accum ea;
+	char ea_inplace_buf[256];	/* the in place accumulator buffer before allocating */
 	struct fy_diag *diag;
 
 	/* streaming event mode */
@@ -133,116 +122,15 @@ static inline bool fy_emit_open_ended(struct fy_emitter *emit)
 }
 
 static inline void
-fy_emit_accum_reset(struct fy_emit_accum *ea)
+fy_emit_output_accum(struct fy_emitter *emit, enum fy_emitter_write_type type, struct fy_emit_accum *ea)
 {
-	ea->next = 0;
-	ea->utf8_count = 0;
-	ea->col = ea->start_col;
-}
+	const char *text;
+	size_t len;
 
-static inline void fy_emit_accum_start(struct fy_emit_accum *ea, enum fy_emitter_write_type type)
-{
-	assert(ea->emit);
-
-	ea->start_col = ea->emit->column;
-	ea->type = type;
+	text = fy_emit_accum_get(ea, &len);
+	if (text && len > 0)
+		fy_emit_write(emit, type, text, len);
 	fy_emit_accum_reset(ea);
-}
-
-static inline void fy_emit_accum_init(struct fy_emit_accum *ea, struct fy_emitter *emit)
-{
-	assert(ea);
-	assert(emit);
-
-	memset(ea, 0, sizeof(*ea));
-
-	ea->emit = emit;
-	ea->accum = ea->inplace;
-	ea->alloc = sizeof(ea->inplace);
-	ea->start_col = ea->emit->column;
-	ea->ts = 8;	/* XXX for now */
-	fy_emit_accum_reset(ea);
-}
-
-static inline void fy_emit_accum_cleanup(struct fy_emit_accum *ea)
-{
-	if (ea->accum && ea->accum != ea->inplace)
-		free(ea->accum);
-}
-
-static inline void fy_emit_accum_finish(struct fy_emit_accum *ea)
-{
-	fy_emit_accum_reset(ea);
-}
-
-int fy_emit_accum_grow(struct fy_emit_accum *ea);
-
-static inline int
-fy_emit_accum_utf8_put_raw(struct fy_emit_accum *ea, int c)
-{
-	size_t w;
-	char *ss;
-	int ret;
-
-	/* grow if needed */
-	w = fy_utf8_width(c);
-	while (w > (ea->alloc - ea->next)) {
-		ret = fy_emit_accum_grow(ea);
-		if (ret != 0)
-			return ret;
-	}
-	ss = fy_utf8_put(ea->accum + ea->next, ea->alloc - ea->next, c);
-	if (!ss)
-		return -1;
-	ea->next += w;
-	ea->utf8_count++;
-
-	return 0;
-}
-
-static inline int
-fy_emit_accum_utf8_put(struct fy_emit_accum *ea, int c, struct fy_token *fyt)
-{
-	int ret;
-
-	if (fy_is_lb_m(c, fy_token_atom_lb_mode(fyt))) {
-		ret = fy_emit_accum_utf8_put_raw(ea, '\n');
-		if (ret)
-			return ret;
-		ea->col = 0;
-	} else if (fy_is_tab(c)) {
-		ret = fy_emit_accum_utf8_put_raw(ea, '\t');
-		if (ret)
-			return ret;
-		ea->col += (ea->ts - (ea->col % ea->ts));
-	} else {
-		ret = fy_emit_accum_utf8_put_raw(ea, c);
-		if (ret)
-			return ret;
-		ea->col++;
-	}
-
-	return 0;
-}
-
-static inline void
-fy_emit_accum_output(struct fy_emit_accum *ea)
-{
-	if (ea->next > 0)
-		fy_emit_write(ea->emit, ea->type, ea->accum, ea->next);
-	fy_emit_accum_reset(ea);
-}
-
-static inline int
-fy_emit_accum_utf8_size(struct fy_emit_accum *ea)
-{
-	return ea->utf8_count;
-}
-
-static inline int
-fy_emit_accum_column(struct fy_emit_accum *ea)
-{
-	return ea->col;
 }
 
 #endif
