40fedb4b01713a6a09d4e96c12afb9eeb150a103
Pantelis Antoniou <pantelis.antoniou@konsulko.com>
2021-11-15 19:46:19 +0200

    composer: Add diagnostic methods
    When we'll start using the composer more extensively these
    diagnostic methods will come in handy.
    
    Signed-off-by: Pantelis Antoniou <pantelis.antoniou@konsulko.com>

--- src/lib/fy-diag.c
+++ src/lib/fy-diag.c
@@ -684,6 +684,73 @@ void fy_document_diag_report(struct fy_document *fyd,
 	va_end(ap);
 }
 
+/* composer */
+
+int fy_composer_vdiag(struct fy_composer *fyc, unsigned int flags,
+		      const char *file, int line, const char *func,
+		      const char *fmt, va_list ap)
+{
+	struct fy_diag_ctx fydc;
+	int rc;
+
+	if (!fyc || !fmt || !fyc->cfg.diag)
+		return -1;
+
+	/* perform the enable tests early to avoid the overhead */
+	if (((flags & FYDF_LEVEL_MASK) >> FYDF_LEVEL_SHIFT) < fyc->cfg.diag->cfg.level)
+		return 0;
+
+	/* fill in fy_diag_ctx */
+	memset(&fydc, 0, sizeof(fydc));
+
+	fydc.level = (flags & FYDF_LEVEL_MASK) >> FYDF_LEVEL_SHIFT;
+	fydc.module = (flags & FYDF_MODULE_MASK) >> FYDF_MODULE_SHIFT;
+	fydc.source_file = file;
+	fydc.source_line = line;
+	fydc.source_func = func;
+	fydc.line = -1;
+	fydc.column = -1;
+
+	rc = fy_vdiag(fyc->cfg.diag, &fydc, fmt, ap);
+
+	return rc;
+}
+
+int fy_composer_diag(struct fy_composer *fyc, unsigned int flags,
+		     const char *file, int line, const char *func,
+		     const char *fmt, ...)
+{
+	va_list ap;
+	int rc;
+
+	va_start(ap, fmt);
+	rc = fy_composer_vdiag(fyc, flags, file, line, func, fmt, ap);
+	va_end(ap);
+
+	return rc;
+}
+
+void fy_composer_diag_vreport(struct fy_composer *fyc,
+			      const struct fy_diag_report_ctx *fydrc,
+			      const char *fmt, va_list ap)
+{
+	if (!fyc || !fyc->cfg.diag || !fydrc || !fmt)
+		return;
+
+	fy_diag_vreport(fyc->cfg.diag, fydrc, fmt, ap);
+}
+
+void fy_composer_diag_report(struct fy_composer *fyc,
+			     const struct fy_diag_report_ctx *fydrc,
+			     const char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	fy_composer_diag_vreport(fyc, fydrc, fmt, ap);
+	va_end(ap);
+}
+
 /* reader */
 
 int fy_reader_vdiag(struct fy_reader *fyr, unsigned int flags,
