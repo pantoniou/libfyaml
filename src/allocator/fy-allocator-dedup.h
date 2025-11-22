fc892f6f993b3e2772f3ee1b38114fb985d1b685
Pantelis Antoniou <pantelis.antoniou@konsulko.com>
2025-11-22 09:44:38 +0200

    tunable tag number for dedup
    Signed-off-by: Pantelis Antoniou <pantelis.antoniou@konsulko.com>

--- src/allocator/fy-allocator-dedup.h
+++ src/allocator/fy-allocator-dedup.h
@@ -78,8 +78,10 @@ struct fy_dedup_allocator {
 	unsigned int bucket_count_bits;
 	size_t dedup_threshold;
 	unsigned int chain_length_grow_trigger;
-	fy_id_bits ids[FY_ID_BITS_ARRAY_COUNT_BITS(FY_DEDUP_TAG_MAX)];
-	struct fy_dedup_tag tags[FY_DEDUP_TAG_MAX];
+	fy_id_bits *ids;
+	unsigned int tag_id_count;
+	struct fy_dedup_tag *tags;
+	unsigned int tag_count;
 };
 
 extern const struct fy_allocator_ops fy_dedup_allocator_ops;
