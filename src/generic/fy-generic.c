716776e9033e01cbcd361bd5eb521a166ed31102
Pantelis Antoniou <pantelis.antoniou@konsulko.com>
2025-12-19 12:11:42 +0200

    sort op
    Signed-off-by: Pantelis Antoniou <pantelis.antoniou@konsulko.com>

--- src/generic/fy-generic.c
+++ src/generic/fy-generic.c
@@ -2298,6 +2298,72 @@ err_out:
 	goto out;
 }
 
+static fy_generic
+fy_generic_op_sort(const struct fy_generic_op_desc *desc,
+		     struct fy_generic_builder *gb, enum fy_gb_op_flags flags,
+		     fy_generic in, const struct fy_generic_op_args *args)
+{
+	struct fy_generic_collection_op_data cod_local, *cod = &cod_local;
+	struct fy_generic_collection col;
+	struct iovec iov[2];
+	size_t i, k;
+	fy_generic out = fy_invalid;
+	int rc;
+
+	rc = fy_generic_collection_op_data_setup(cod, gb, flags,
+			in, FYGT_INVALID, args,
+			FYGCODSF_MAP_ITEM_COUNT_NO_MULT2 |
+			FYGCODSF_NEED_WORK_IN_ITEMS | FYGCODSF_NEED_WORK_ITEMS_EXPANDED |
+			FYGCODSF_NEED_COPY_WORK_IN_ITEMS | FYGCODSF_NEED_COPY_WORK_ITEMS_EXPANDED);
+	if (rc)
+		return fy_invalid;
+
+	if (args->common.count && !args->common.items)
+		goto err_out;
+
+	/* nothing? return empty */
+	if (cod->work_item_all_count == 0)  {
+		out = cod->type == FYGT_SEQUENCE ? fy_seq_empty : fy_map_empty;
+		goto out;
+	}
+
+	k = cod->work_item_all_count;
+	if (cod->type == FYGT_SEQUENCE) {
+		/* the extra arguments */
+		for (i = 0; i < cod->item_count; i++) {
+			if (!fy_generic_is_sequence(cod->items[i]))
+				goto err_out;
+		}
+
+		qsort(cod->work_items_all, k, sizeof(fy_generic), fy_generic_seqmap_qsort_cmp);
+	} else {
+		/* the extra arguments */
+		for (i = 0; i < cod->item_count; i++) {
+			if (!fy_generic_is_mapping(cod->items[i]))
+				goto err_out;
+		}
+
+		qsort(cod->work_items_all, k / 2, 2 * sizeof(fy_generic), fy_generic_seqmap_qsort_cmp);
+	}
+
+	/* the collection header */
+	col.count = cod->type == FYGT_SEQUENCE ? k : (k / 2);
+	iov[0].iov_base = &col;
+	iov[0].iov_len = sizeof(col);
+	/* the content */
+	iov[1].iov_base = cod->work_items_all;
+	iov[1].iov_len = MULSZ(col.count, cod->col_item_size);
+
+	out = fy_generic_collection_op_data_out(cod, iov, ARRAY_SIZE(iov));
+out:
+	fy_generic_collection_op_data_cleanup(cod);
+	return out;
+
+err_out:
+	out = fy_invalid;
+	goto out;
+}
+
 static const struct fy_generic_op_desc op_descs[FYGBOP_COUNT] = {
 	[FYGBOP_CREATE_INV] = {
 		.op = FYGBOP_CREATE_INV,
@@ -2446,6 +2512,13 @@ static const struct fy_generic_op_desc op_descs[FYGBOP_COUNT] = {
 		.out_mask = FYGTM_SEQUENCE,
 		.handler = fy_generic_op_unique,
 	},
+	[FYGBOP_SORT] = {
+		.op = FYGBOP_SORT,
+		.op_name = "sort",
+		.in_mask = FYGTM_COLLECTION,	// sort works on mappings too
+		.out_mask = FYGTM_COLLECTION,
+		.handler = fy_generic_op_sort,
+	},
 };
 
 fy_generic
@@ -2513,13 +2586,6 @@ fy_generic_op_args(struct fy_generic_builder *gb, enum fy_gb_op_flags flags,
 			return in;
 		if (!items)
 			return fy_invalid;
-		break;
-
-	case FYGBOP_SORT:
-		if (count && !items)
-			return fy_invalid;
-		need_work_items = true;
-		need_copy_work_items = true;
 		break;
 
 	case FYGBOP_FILTER:
@@ -2636,9 +2702,7 @@ fy_generic_op_args(struct fy_generic_builder *gb, enum fy_gb_op_flags flags,
 		if (flags & FYGBOPF_MAP_ITEM_COUNT)
 			count /= 2;
 
-		item_count = (op != FYGBOP_CONTAINS &&
-			      op != FYGBOP_SORT &&
-			      op != FYGBOP_FILTER && op != FYGBOP_MAP && op != FYGBOP_MAP_FILTER &&
+		item_count = (op != FYGBOP_FILTER && op != FYGBOP_MAP && op != FYGBOP_MAP_FILTER &&
 			      op != FYGBOP_REDUCE && op != FYGBOP_GET_AT_PATH && op != FYGBOP_SET_AT_PATH) ?
 					MULSZ(count, 2) : count;
 		col_mark = FY_MAP_V;
@@ -2687,7 +2751,6 @@ fy_generic_op_args(struct fy_generic_builder *gb, enum fy_gb_op_flags flags,
 	if (need_work_items) {
 
 		switch (op) {
-		case FYGBOP_SORT:
 		case FYGBOP_FILTER:
 		case FYGBOP_MAP:
 		case FYGBOP_MAP_FILTER:
@@ -2761,38 +2824,6 @@ fy_generic_op_args(struct fy_generic_builder *gb, enum fy_gb_op_flags flags,
 do_operation:
 
 	switch (op) {
-	case FYGBOP_SORT:
-		assert(work_items);
-		assert(items_mod);
-		assert(need_copy_work_items);
-
-		/* the collection header */
-		if (type == FYGT_SEQUENCE) {
-			col.count = work_item_count;
-			if (!col.count) {
-				/* nothing? */
-				out = fy_seq_empty;
-				goto out;
-			}
-			qsort(work_items, work_item_count, sizeof(*work_items), fy_generic_seqmap_qsort_cmp);
-		} else {
-			col.count = work_item_count / 2;
-			if (!col.count) {
-				/* nothing? */
-				out = fy_map_empty;
-				goto out;
-			}
-			qsort(work_items, work_item_count / 2, sizeof(*work_items) * 2, fy_generic_seqmap_qsort_cmp);
-		}
-
-		iov[0].iov_base = &col;
-		iov[0].iov_len = sizeof(col);
-		/* the content */
-		iov[1].iov_base = work_items;
-		iov[1].iov_len = MULSZ(col.count, col_item_size);
-		iovcnt = 2;
-		break;
-
 	case FYGBOP_FILTER:
 	case FYGBOP_MAP:
 	case FYGBOP_MAP_FILTER:
