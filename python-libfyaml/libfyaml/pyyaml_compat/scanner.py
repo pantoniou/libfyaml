fdcc4b079221b07f989fee73c96b988f3ae056f1
Pantelis Antoniou <pantelis.antoniou@konsulko.com>
2026-01-16 12:18:35 +0200

    commited claudes work
    Signed-off-by: Pantelis Antoniou <pantelis.antoniou@konsulko.com>

---
+++ python-libfyaml/libfyaml/pyyaml_compat/scanner.py
@@ -0,0 +1,14 @@
+"""PyYAML scanner module compatibility stubs.
+
+Provides Scanner classes for API compatibility.
+"""
+
+
+class ScannerError(Exception):
+    """YAML scanner error."""
+    pass
+
+
+class Scanner:
+    """YAML scanner stub."""
+    pass
