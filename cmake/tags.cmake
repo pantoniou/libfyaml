fbf69502757546bfd3609f7056a399c009ad0110
Pantelis Antoniou <pantelis.antoniou@konsulko.com>
2026-06-02 09:46:51 +0300

    cmake: namespace developer helper targets
    Make libfyaml easily vendorable by namespacing all the custom
    targets using a libfyaml- prefix.
    
    If the build is a top level one, then add aliases for the original
    named targets.
    
    Signed-off-by: Pantelis Antoniou <pantelis.antoniou@konsulko.com>

--- cmake/tags.cmake
+++ cmake/tags.cmake
@@ -2,9 +2,9 @@ find_program(CTAGS ctags)
 
 if(CTAGS)
     # Name of the build directory (for exclusion)
-    get_filename_component(_build_dir_name "${CMAKE_BINARY_DIR}" NAME)
+    get_filename_component(_build_dir_name "${CMAKE_CURRENT_BINARY_DIR}" NAME)
 
-    add_custom_target(ctags
+    add_custom_target(libfyaml-ctags
         COMMAND ${CTAGS}
             -R
             --exclude=${_build_dir_name}
@@ -13,9 +13,13 @@ if(CTAGS)
             --extra=+q
             --c-kinds=+lpx
             --fields=afikmsSzt
-            -f "${CMAKE_BINARY_DIR}/tags"
-            "${CMAKE_SOURCE_DIR}"
-        WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+            -f "${CMAKE_CURRENT_BINARY_DIR}/tags"
+            "${CMAKE_CURRENT_SOURCE_DIR}"
+        WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
         VERBATIM
     )
+
+    if(PROJECT_IS_TOP_LEVEL AND NOT TARGET ctags)
+        add_custom_target(ctags DEPENDS libfyaml-ctags)
+    endif()
 endif()
