43ea20c12d2a6ba3fd82239bfc170ab35ab46391
Pantelis Antoniou <pantelis.antoniou@konsulko.com>
2026-06-19 12:46:51 +0300

    endian: Add portable byte-order conversion helpers
    Provide the full htobe/htole/betoh/letoh family for 16/32/64-bit
    values directly in libfyaml-endian.h if the platform does not
    provide them.
    
    Signed-off-by: Pantelis Antoniou <pantelis.antoniou@konsulko.com>

--- include/libfyaml/libfyaml-endian.h
+++ include/libfyaml/libfyaml-endian.h
@@ -38,6 +38,8 @@
 #ifndef LIBFYAML_ENDIAN_H
 #define LIBFYAML_ENDIAN_H
 
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -90,6 +92,140 @@ extern "C" {
 #ifndef bswap_8
 #define bswap_8(x) (x)
 #endif
+
+/*
+ * Byte-swap primitives.
+ */
+#if defined(__GNUC__) || defined(__clang__)
+
+static inline uint16_t fy_bswap16(uint16_t x) { return __builtin_bswap16(x); }
+static inline uint32_t fy_bswap32(uint32_t x) { return __builtin_bswap32(x); }
+static inline uint64_t fy_bswap64(uint64_t x) { return __builtin_bswap64(x); }
+
+#elif defined(_MSC_VER)
+
+#include <stdlib.h>
+static inline uint16_t fy_bswap16(uint16_t x) { return _byteswap_ushort(x); }
+static inline uint32_t fy_bswap32(uint32_t x) { return _byteswap_ulong(x); }
+static inline uint64_t fy_bswap64(uint64_t x) { return _byteswap_uint64(x); }
+
+#else
+
+static inline uint16_t fy_bswap16(uint16_t x)
+{
+	return (x << 8) | (x >> 8);
+}
+
+static inline uint32_t fy_bswap32(uint32_t x)
+{
+	return ((x & (uint32_t)0x000000ffUL) << 24) |
+	       ((x & (uint32_t)0x0000ff00UL) <<  8) |
+	       ((x & (uint32_t)0x00ff0000UL) >>  8) |
+	       ((x & (uint32_t)0xff000000UL) >> 24);
+}
+
+static inline uint64_t fy_bswap64(uint64_t x)
+{
+	return ((x & (uint64_t)0x00000000000000ffULL) << 56) |
+	       ((x & (uint64_t)0x000000000000ff00ULL) << 40) |
+	       ((x & (uint64_t)0x0000000000ff0000ULL) << 24) |
+	       ((x & (uint64_t)0x00000000ff000000ULL) <<  8) |
+	       ((x & (uint64_t)0x000000ff00000000ULL) >>  8) |
+	       ((x & (uint64_t)0x0000ff0000000000ULL) >> 24) |
+	       ((x & (uint64_t)0x00ff000000000000ULL) >> 40) |
+	       ((x & (uint64_t)0xff00000000000000ULL) >> 56);
+}
+
+#endif
+
+/*
+ * host <-> little/big-endian conversion helpers.
+ *
+ * If the platform already provides these use them, otherwise build them
+ */
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+
+#ifndef htole16
+#define htole16(x) ((uint16_t)(x))
+#endif
+#ifndef le16toh
+#define le16toh(x) ((uint16_t)(x))
+#endif
+#ifndef htobe16
+#define htobe16(x) fy_bswap16((uint16_t)(x))
+#endif
+#ifndef be16toh
+#define be16toh(x) fy_bswap16((uint16_t)(x))
+#endif
+
+#ifndef htole32
+#define htole32(x) ((uint32_t)(x))
+#endif
+#ifndef le32toh
+#define le32toh(x) ((uint32_t)(x))
+#endif
+#ifndef htobe32
+#define htobe32(x) fy_bswap32((uint32_t)(x))
+#endif
+#ifndef be32toh
+#define be32toh(x) fy_bswap32((uint32_t)(x))
+#endif
+
+#ifndef htole64
+#define htole64(x) ((uint64_t)(x))
+#endif
+#ifndef le64toh
+#define le64toh(x) ((uint64_t)(x))
+#endif
+#ifndef htobe64
+#define htobe64(x) fy_bswap64((uint64_t)(x))
+#endif
+#ifndef be64toh
+#define be64toh(x) fy_bswap64((uint64_t)(x))
+#endif
+
+#else /* big endian */
+
+#ifndef htole16
+#define htole16(x) fy_bswap16((uint16_t)(x))
+#endif
+#ifndef le16toh
+#define le16toh(x) fy_bswap16((uint16_t)(x))
+#endif
+#ifndef htobe16
+#define htobe16(x) ((uint16_t)(x))
+#endif
+#ifndef be16toh
+#define be16toh(x) ((uint16_t)(x))
+#endif
+
+#ifndef htole32
+#define htole32(x) fy_bswap32((uint32_t)(x))
+#endif
+#ifndef le32toh
+#define le32toh(x) fy_bswap32((uint32_t)(x))
+#endif
+#ifndef htobe32
+#define htobe32(x) ((uint32_t)(x))
+#endif
+#ifndef be32toh
+#define be32toh(x) ((uint32_t)(x))
+#endif
+
+#ifndef htole64
+#define htole64(x) fy_bswap64((uint64_t)(x))
+#endif
+#ifndef le64toh
+#define le64toh(x) fy_bswap64((uint64_t)(x))
+#endif
+#ifndef htobe64
+#define htobe64(x) ((uint64_t)(x))
+#endif
+#ifndef be64toh
+#define be64toh(x) ((uint64_t)(x))
+#endif
+
+#endif /* __BYTE_ORDER */
 
 #ifdef __cplusplus
 }
