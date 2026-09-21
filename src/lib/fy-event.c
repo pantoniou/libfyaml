bcbd8ea442b50cab332c9027bcbb39aa6311a89d
Pantelis Antoniou <pantelis.antoniou@konsulko.com>
2026-09-21 11:17:48 +0300

    event: clear the document state of a DOCUMENT-START event
    The cleanup of the event released a document state that the creation
    of the event had not set yet, when the allocation of the default state
    failed.
    
    Fixes #374
    
    Signed-off-by: Pantelis Antoniou <pantelis.antoniou@konsulko.com>

--- src/lib/fy-event.c
+++ src/lib/fy-event.c
@@ -256,6 +256,7 @@ fy_eventp_vcreate_internal(struct fy_eventp_list *recycled_list, struct fy_diag 
 		break;
 	case FYET_DOCUMENT_START:
 		fye->document_start.document_start = NULL;
+		fye->document_start.document_state = NULL;
 		fyds_new = fy_document_state_default(fy_document_state_version(fyds), NULL);	/* start with the default state */
 		if (!fyds_new) {
 			fy_error(diag, "fy_document_state_alloc() failed\n");
